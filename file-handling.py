6c0040d8e7b9be8efec0bd4ebece56276fd61d78
Ayroid <ayushsk0000@gmail.com>
2025-01-01 17:56:53 +0530

    feat: update file handling

--- file-handling.py
+++ file-handling.py
@@ -7,7 +7,11 @@ def move_files_by_type(source_folder, destination_folder, file_extension):
         os.makedirs(destination_folder)
     for filename in os.listdir(source_folder):
         if filename.endswith(file_extension):
-            shutil.move(os.path.join(source_folder, filename), destination_folder)
+            source_path = os.path.join(source_folder, filename)
+            destination_path = os.path.join(destination_folder, filename)
+            if os.path.exists(destination_path):
+                os.remove(destination_path)
+            shutil.move(source_path, destination_folder)
             print(f"Moved {filename} to {destination_folder}")
 
 
@@ -20,3 +24,4 @@ move_files_by_type("/home/ayroid/Downloads", "/home/ayroid/Downloads/videos", ".
 move_files_by_type("/home/ayroid/Downloads", "/home/ayroid/Downloads/json", ".json")
 move_files_by_type("/home/ayroid/Downloads", "/home/ayroid/Downloads/excels", ".xlsx")
 move_files_by_type("/home/ayroid/Downloads", "/home/ayroid/Downloads/svgs", ".svg")
+move_files_by_type("/home/ayroid/Downloads", "/home/ayroid/Downloads/docx", ".docx")
